4a9689c196f51a777b3a91d155fd25d14c790d2a
Carolina Moutella <ola@carolinamoutella.com>
2023-07-14 10:03:33 -0300

    feat(Title): isolete as component

---
+++ src/pageComponents/home/Title.tsx
@@ -0,0 +1,18 @@
+const Title = () => {
+  return (
+    <section className="container">
+      <h1 className="text-[35px] sm:text-[69px] font-bold text-gray-light font-changa-one /// max-w-[240px] sm:max-w-[600px] /// z-2 mt-[30px] mx-auto relative">
+        Designer{" "}
+        <span
+          className="text-primary-light text-[120px] sm:text-[220px] font-extra-bold font-paytone /// absolute -z-1 end-[25%] top-[30%] translate-x-[-50%] translate-y-[-50%]"
+          style={{ zIndex: -1 }}
+        >
+          &
+        </span>{" "}
+        Desenvolvedora
+      </h1>
+    </section>
+  );
+};
+
+export default Title;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,26 +2,18 @@ import Image from "next/image";
 
 import { Default as Template } from "@/templates";
 
+//Components
+import Section from "@/pageComponents/home/Section";
+import Title from "@/pageComponents/home/Title";
+
 // Assets
 import mockupIlustration from "@/assets/images/home/mockup_ilustration.svg";
 import notebookIlustration from "@/assets/images/home/notebook_ilustration.svg";
-import Section from "@/pageComponents/home/Section";
 
 export default function Home() {
   return (
     <Template>
-      <section className="page-header">
-        <div className="container">
-          <h1 className="text-[35px] sm:text-[69px] font-bold text-gray-light font-changa-one /// max-w-[240px] sm:max-w-[600px] /// z-2 mt-[30px] mx-auto relative">
-            Designer{" "}
-            <span className="text-primary-light text-[120px] sm:text-[220px] font-extra-bold font-paytone /// absolute -z-1 start-0 top-[30%] translate-x-[-50%] translate-y-[-50%]">
-              &
-            </span>{" "}
-            Desenvolvedora
-          </h1>
-        </div>
-      </section>
-
+      <Title />
       <Section
         description="Trabalho com design gráfico voltado para a web, criando peças para
               marketing digital e projetos completos de UI."
